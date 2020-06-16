f7d37d241019a02524e1ebef970bac4644d2a10e
Prete <martin.prete@sanger.ac.uk>
2020-06-16 18:59:24 +0100

    Update poststart.sh
    Remove deletion of previous notebooks/data.
    Install IRkernel.
    Create myenv if missing

--- poststart.sh
+++ poststart.sh
@@ -1,25 +1,23 @@
 #!/usr/bin/env bash
 
-# copy default templates
-if [ -d git-notebooks ]; then
-    rm -rf git-notebooks
-fi
-git clone https://github.com/cellgeni/notebooks git-notebooks
-cp -Rf git-notebooks/files/. .
-rm -rf git-notebooks
+
+# create default environment
 if [ ! -d my-conda-envs/myenv ]; then
     conda create --clone base --name myenv
+    source activate myenv
+    python -m ipykernel install --user --name=myenv
+fi
+if [ ! -d .bashrc ]; then
+    echo "source activate myenv" > .bashrc
 fi
 
 Rscript -e 'dir.create(path = Sys.getenv("R_LIBS_USER"), showWarnings = FALSE, recursive = TRUE)'
 Rscript -e '.libPaths( c( Sys.getenv("R_LIBS_USER"), .libPaths() ) )'
+Rscript -e 'IRkernel::installspec()'
 
 # create matching folders to mount the farm
 sudo mkdir -p /nfs
 sudo mkdir -p /lustre
 sudo mkdir -p /warehouse
-
-# start docker service
-sudo service docker start
 
 export USER=jovyan
