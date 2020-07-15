4f03c583cb164176918c9b9e9218eda7d8908014
Prete <martin.prete@sanger.ac.uk>
2020-07-15 18:33:52 +0100

    Update poststart.sh

--- poststart.sh
+++ poststart.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+if [ ! -d .condarc ]; then
+    git clone https://github.com/cellgeni/notebooks git-notebooks
+    cp -Rf git-notebooks/files/. .
+    rm -rf git-notebooks
+fi
 
 # create default environment
 if [ ! -d my-conda-envs/myenv ]; then
     conda create --clone base --name myenv
     source activate myenv
     python -m ipykernel install --user --name=myenv
-fi
-if [ ! -d .bashrc ]; then
-    echo "source activate myenv" > .bashrc
 fi
 
 Rscript -e 'dir.create(path = Sys.getenv("R_LIBS_USER"), showWarnings = FALSE, recursive = TRUE)'
